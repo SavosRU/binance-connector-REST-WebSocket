eeebac2256d2f6587972d6807d0e27c0d14f20d5
M.Hasan Jabbari <39640448+mhasanjb@users.noreply.github.com>
2022-11-26 12:07:56 +0000

    coinm - dapi

---
+++ core/coinm.js
@@ -0,0 +1,58 @@
+import { Websocket } from "./websocket.js"
+import { Http } from "./http.js"
+import("../types/futures.type.js")
+
+export class Futures {
+    
+    ApiMap = {
+        baseURL: "https://dapi.binance.com",
+        baseURLTest: "https://testnet.binancefuture.com",
+        wsBaseURL: "wss://dstream.binance.com",
+        wsBaseURLTest: "wss://dstream.binancefuture.com",
+    }
+
+    timestamp = Date.now()
+
+    /**
+     * @param {} options
+     */
+    constructor(options = {}) {
+
+        let OPTIONS = {
+            ...options,
+            ...this.ApiMap,
+            timestamp: this.timestamp,
+        }
+
+        // Coming from constructor
+        this.api_key    = options.api_key
+        this.api_secret = options.api_secret
+        this.recvWindow = options.recvWindow
+        this.isTestNet  = options.isTestNet
+
+        // Default values
+        this.recvWindow = this.recvWindow ?? 5000
+        this.isTestNet  = this.isTestNet  ?? false
+        
+        if (!OPTIONS.recvWindow) {
+            OPTIONS.recvWindow = this.recvWindow
+        }
+
+        // Websocket
+        this.ws = new Websocket(OPTIONS)
+        
+        // Utils
+        this.http = new Http(OPTIONS)
+        
+    }
+
+    // ########### Public
+    /**
+     * @param {} [params]
+     */
+    async ping(params) {
+        return await this.http.privateGET("/dapi/v1/ping", params)
+    }
+    
+    // ########### Private
+}
