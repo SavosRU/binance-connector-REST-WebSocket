3859ecbbf325d446a6280bda1c71dfcf39584f4b
M.Hasan Jabbari <39640448+mhasanjb@users.noreply.github.com>
2022-11-26 12:19:14 +0000

    eu options

---
+++ core/euoptions.js
@@ -0,0 +1,70 @@
+import { Websocket } from "./websocket.js"
+import { Http } from "./http.js"
+import("../types/euoptions.type.js")
+
+export class EuOptions {
+    
+    ApiMap = {
+        baseURL: "https://eapi.binance.com",
+        wsBaseURL: "wss://nbstream.binance.com/eoptions/",
+    }
+
+    timestamp = Date.now()
+
+    /**
+     * @param { EuOptionsConstructor } options
+     */
+    constructor(options = {}) {
+
+        options.isTestNet = false
+
+        let OPTIONS = {
+            ...options,
+            ...this.ApiMap,
+            timestamp: this.timestamp,
+        }
+
+        // Coming from constructor
+        this.api_key    = options.api_key
+        this.api_secret = options.api_secret
+        this.recvWindow = options.recvWindow
+
+        // Default values
+        this.recvWindow = this.recvWindow ?? 5000
+        
+        if (!OPTIONS.recvWindow) {
+            OPTIONS.recvWindow = this.recvWindow
+        }
+
+        // Websocket
+        this.ws = new Websocket(OPTIONS)
+        
+        // Utils
+        this.http = new Http(OPTIONS)
+    }
+
+    // ########################################### Market Data Endpoints
+    // ########### Public
+    /**
+     * @param {} [params]
+     */
+    async ping(params) {
+        return await this.http.privateGET("/eapi/v1/ping", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async time(params) {
+        return await this.http.privateGET("/eapi/v1/time", params)
+    }
+    // ########### Private
+    
+    // ########################################### Account/Trades Endpoints
+    // ########### Public
+    // ########### Private
+
+    // ########################################### Market Maker Endpoints
+    // ########### Public
+    // ########### Private
+}
---
+++ types/euoptions.type.js
@@ -0,0 +1,7 @@
+/**
+ * CoinM constructor
+ * @typedef {Object} EuOptionsConstructor
+ * @property {String} [api_key]
+ * @property {String} [api_secret]
+ * @property {Number} [recvWindow]
+ */
