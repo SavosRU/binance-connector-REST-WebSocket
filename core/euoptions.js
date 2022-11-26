971ff821449a42bc94e7e6cee113b8bcf2d60a2a
M.Hasan Jabbari <39640448+mhasanjb@users.noreply.github.com>
2022-11-26 13:55:11 +0000

    euoptions - finished

--- core/euoptions.js
+++ core/euoptions.js
@@ -49,30 +49,232 @@ export class EuOptions {
      * @param {} [params]
      */
     async ping(params) {
-        return await this.http.privateGET("/eapi/v1/ping", params)
+        return await this.http.publicGET("/eapi/v1/ping", params)
     }
 
     /**
      * @param {} [params]
      */
     async time(params) {
-        return await this.http.privateGET("/eapi/v1/time", params)
+        return await this.http.publicGET("/eapi/v1/time", params)
     }
 
     /**
      * @param {} [params]
      */
-    async test(params) {
-        return await this.http.privateGET("/eapi/v1/klines", params)
-        // return await this.http.privateGET("/eapi/v1/exchangeInfo", params)
+    async exchangeInfo(params) {
+        return await this.http.publicGET("/eapi/v1/exchangeInfo", params)
     }
-    // ########### Private
-    
-    // ########################################### Account/Trades Endpoints
-    // ########### Public
-    // ########### Private
 
-    // ########################################### Market Maker Endpoints
-    // ########### Public
+    /**
+     * @param {} [params]
+     */
+    async depth(params) {
+        return await this.http.publicGET("/eapi/v1/depth", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async trades(params) {
+        return await this.http.publicGET("/eapi/v1/trades", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async historicalTrades(params) {
+        return await this.http.publicGET("/eapi/v1/historicalTrades", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async klines(params) {
+        return await this.http.publicGET("/eapi/v1/klines", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async mark(params) {
+        return await this.http.publicGET("/eapi/v1/mark", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async ticker(params) {
+        return await this.http.publicGET("/eapi/v1/ticker", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async index(params) {
+        return await this.http.publicGET("/eapi/v1/index", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async exerciseHistory(params) {
+        return await this.http.publicGET("/eapi/v1/exerciseHistory", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+    async openInterest(params) {
+        return await this.http.publicGET("/eapi/v1/openInterest", params)
+    }
+    // ########################################### Account/Trades Endpoints
     // ########### Private
+    /**
+     * @param {} [params]
+     */
+     async account(params) {
+        return await this.http.privateGET("/eapi/v1/account", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+     async newTransfer(params) {
+        return await this.http.privatePOST("/eapi/v1/transfer", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+     async newOrder(params) {
+        return await this.http.privatePOST("/eapi/v1/order", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+     async newBatchOrders(params) {
+        return await this.http.privatePOST("/eapi/v1/batchOrders", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+     async deleteOrder(params) {
+        return await this.http.privateDELETE("/eapi/v1/order", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+     async deleteBatchOrders(params) {
+        return await this.http.privateDELETE("/eapi/v1/batchOrders", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+     async deleteAllOpenOrders(params) {
+        return await this.http.privateDELETE("/eapi/v1/allOpenOrders", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+     async deleteAllOpenOrdersByUnderlying(params) {
+        return await this.http.privateDELETE("/eapi/v1/allOpenOrdersByUnderlying", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+     async openOrders(params) {
+        return await this.http.privateGET("/eapi/v1/openOrders", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+     async historyOrders(params) {
+        return await this.http.privateGET("/eapi/v1/historyOrders", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+     async position(params) {
+        return await this.http.privateGET("/eapi/v1/position", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+     async userTrades(params) {
+        return await this.http.privateGET("/eapi/v1/userTrades", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+     async exerciseRecord(params) {
+        return await this.http.privateGET("/eapi/v1/exerciseRecord", params)
+    }
+
+    /**
+     * @param {} [params]
+     */
+     async bill(params) {
+        return await this.http.privateGET("/eapi/v1/bill", params)
+    }
+    // ########################################### Market Maker Endpoints
+    // ########### Private
+    /**
+     * @param {} [params]
+     */
+     async marginAccount(params) {
+        return await this.http.privateGET("/eapi/v1/marginAccount", params)
+    }
+    
+    /**
+     * @param {} [params]
+     */
+     async newMmpSet(params) {
+        return await this.http.privatePOST("/eapi/v1/mmpSet", params)
+    }
+    
+    /**
+     * @param {} [params]
+     */
+     async mmp(params) {
+        return await this.http.privateGET("/eapi/v1/mmp", params)
+    }
+    
+    /**
+     * @param {} [params]
+     */
+     async newMmpReset(params) {
+        return await this.http.privatePOST("/eapi/v1/mmpReset", params)
+    }
+    
+    /**
+     * @param {} [params]
+     */
+     async newCountdownCancelAll(params) {
+        return await this.http.privatePOST("/eapi/v1/countdownCancelAll", params)
+    }
+    
+    /**
+     * @param {} [params]
+     */
+     async countdownCancelAll(params) {
+        return await this.http.privateGET("/eapi/v1/countdownCancelAll", params)
+    }
+    
+    /**
+     * @param {} [params]
+     */
+     async newCountdownCancelAllHeartBeat(params) {
+        return await this.http.privateGET("/eapi/v1/countdownCancelAllHeartBeat", params)
+    }
 }
