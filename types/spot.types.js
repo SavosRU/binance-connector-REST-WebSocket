9d90804c855d50dc945d870d27271babf3213555
M.Hasan Jabbari <39640448+mhasanjb@users.noreply.github.com>
2022-11-27 09:38:56 +0000

    wallet

--- types/spot.types.js
+++ types/spot.types.js
@@ -13,11 +13,207 @@
 // ########### ENUM
 // 
 
-// ########### Public
 /**
  * @typedef {Object} SpotJustRecvWindow
  * @property {Number} [recvWindow]
  */
 
-// ########### Private
-// 
+// ########### Wallet Endpoints
+/**
+ * @typedef {Object} SpotSystemStatus
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotCapitalConfigGetall
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotAccountSnapshot
+ * @property {"SPOT" | "MARGIN" | "FUTURES"} type
+ * @property {Number} [startTime]
+ * @property {Number} [endTime]
+ * @property {Number} [limit]
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotPostAccountDisableFastWithdrawSwitch
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotPostAccountEnableFastWithdrawSwitch
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotPostCapitalWithdrawApply
+ * @property {String} coin
+ * @property {String} withdrawOrderId
+ * @property {String} [network]
+ * @property {String} address
+ * @property {String} [addressTag]
+ * @property {Number} amount
+ * @property {Boolean} [transactionFeeFlag]
+ * @property {String} [name]
+ * @property {0 | 1} [walletType]
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotCapitalDepositHisrec
+ * @property {String} [coin]
+ * @property {0 | 1 | 6} [status]
+ * @property {Number} [startTime]
+ * @property {Number} [endTime]
+ * @property {Number} [offset]
+ * @property {Number} [limit]
+ * @property {String} [txId]
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotCapitalWithdrawHistory
+ * @property {String} [coin]
+ * @property {String} [withdrawOrderId]
+ * @property {0 | 1 | 2 | 3 | 4 | 5 | 6} [status]
+ * @property {Number} [offset]
+ * @property {Number} [limit]
+ * @property {Number} [startTime]
+ * @property {Number} [endTime]
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotCapitalDepositAddress
+ * @property {String} coin
+ * @property {String} [network]
+ * @property {Number} [recvWindow]
+ */
+/**
+ * @typedef {Object} SpotAccountStatus
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotAccountApiTradingStatus
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotAssetDribblet
+ * @property {Number} [startTime]
+ * @property {Number} [endTime]
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotPostAssetDustBtc
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotPostAssetDust
+ * @property {Array} asset
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotAssetAssetDividend
+ * @property {String} [asset]
+ * @property {Number} [startTime]
+ * @property {Number} [endTime]
+ * @property {Number} [limit]
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotAssetAssetDetail
+ * @property {String} [asset]
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotAssetTradeFee
+ * @property {String} [symbol]
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotPostAssetTransfer
+ * @property {} type #ENUM
+ * @property {String} asset
+ * @property {Number} amount
+ * @property {String} [fromSymbol]
+ * @property {String} [toSymbol]
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotAssetTransfer
+ * @property {} type #ENUM
+ * @property {Number} [startTime]
+ * @property {Number} [endTime]
+ * @property {Number} [current]
+ * @property {Number} [size]
+ * @property {String} [fromSymbol]
+ * @property {String} [toSymbol]
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotPostAssetGetFundingAsset
+ * @property {String} [asset]
+ * @property {true | false} [needBtcValuation] String true | false
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotPostAssetGetUserAsset
+ * @property {String} [asset]
+ * @property {Boolean} [needBtcValuation]
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotPostAssetConvertTransfer
+ * @property {String} clientTranId
+ * @property {String} asset
+ * @property {Number} amount
+ * @property {String} targetAsset
+ * @property {"MAIN" | "CARD"} [accountType]
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotAssetConvertTransferQueryByPage
+ * @property {Number} [tranId]
+ * @property {String} [clientTranId]
+ * @property {String} [asset]
+ * @property {Number} startTime
+ * @property {Number} endTime
+ * @property {"MAIN" | "CARD"} [accountType]
+ * @property {Number} [current]
+ * @property {Number} [size]
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotAssetLedgerTransferCloudMiningQueryByPag
+ * @property {Number} [tranId]
+ * @property {String} [clientTranId]
+ * @property {String} [asset]
+ * @property {Number} startTime
+ * @property {Number} endTime
+ * @property {Number} [current]
+ * @property {Number} [size]
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} SpotAccountApiRestrictions
+ * @property {Number} [recvWindow]
+ */
+
