b005704a703c3f39ecc0208a6a5bf396cc3a22b3
M.Hasan Jabbari <39640448+mhasanjb@users.noreply.github.com>
2022-11-28 16:49:03 +0000

    coinm

--- types/coinm.type.js
+++ types/coinm.type.js
@@ -6,3 +6,173 @@
  * @property {Number} [recvWindow]
  * @property {Boolean} [isTestNet]
  */
+// ################################### API
+// ########### ENUM
+/**
+ * @typedef {"DELIVERY_CONTRACT" | "PERPETUAL_CONTRACT"} EnumCoinmSymbolType
+ */
+
+/**
+ * @typedef {"PERPETUAL" | "CURRENT_QUARTER" | "NEXT_QUARTER" | "CURRENT_QUARTER_DELIVERING" | "NEXT_QUARTER_DELIVERING" | "PERPETUAL_DELIVERING"} EnumCoinmContractType
+ */
+ 
+/**
+ * @typedef {"PENDING_TRADING" | "TRADING" | "PRE_DELIVERING" | "DELIVERING" | "DELIVERED"} EnumCoinmContractStatus
+ */
+ 
+/**
+ * @typedef {"NEW" | "PARTIALLY_FILLED" | "FILLED" | "CANCELED" | "EXPIRED"} EnumCoinmOrderStatus
+ */
+
+/**
+ * @typedef {"LIMIT" | "MARKET" | "STOP" | "STOP_MARKET" | "TAKE_PROFIT" | "TAKE_PROFIT_MARKET" | "TRAILING_STOP_MARKET"} EnumCoinmOrderType
+ */
+ 
+/**
+ * @typedef {"BUY" | "SELL"} EnumCoinmOrderSide
+ */
+
+/**
+ * @typedef {"BOTH" | "LONG" | "SHORT"} EnumCoinmPositionSide
+ */
+ 
+/**
+ * @typedef {"GTC" | "IOC" | "FOK" | "GTX"} EnumCoinmTimeInForce
+ */
+ 
+/**
+ * @typedef {"MARK_PRICE" | "CONTRACT_PRICE"} EnumCoinmWorkingType
+ */
+ 
+/**
+ * @typedef {"ACK" | "RESULT"} EnumCoinmNewOrderRespType
+ */
+
+/**
+ * @typedef {"1m" | "3m" | "5m" | "15m" | "30m" | "1h" | "2h" | "4h" | "6h" | "8h" | "12h" | "1d" | "3d" | "1w" | "1M"} EnumCoinmIntervals
+ */
+
+
+/**
+ * @typedef {"REQUEST_WEIGHT" | "ORDERS"} EnumCoinmRateLimitType
+ */
+
+/**
+ * @typedef {"MINUTE"} EnumCoinmRateLimitInterval
+ */
+// ########### Market Data
+
+/**
+ * @typedef {Object} CoinmPing
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
+
+/**
+ * @typedef {Object} Coinm
+ * @property {Number} [recvWindow]
+ */
